83ea84c6f1acbbc4cdffd1ac961e74607929dc33
Satyam <nikamsatyam187@gmail.com>
2025-11-13 16:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-//complete this code
-class Animal {}
+// Animal Class
+class Animal {
+    private _species: string;
 
-class Dog extends Animal {}
+    constructor(species: string) {
+        this._species = species;
+    }
 
-class Cat extends Animal {}
+    // Getter for species
+    get species(): string {
+        return this._species;
+    }
 
+    // Method to make sound
+    makeSound(): void {
+        console.log(`The ${this._species} makes a sound`);
+    }
+}
+
+// Cat Class
+class Cat extends Animal {
+    constructor() {
+        super("cat");
+    }
+
+    // Method to purr
+    purr(): void {
+        console.log("purr");
+    }
+}
+
+// Dog Class
+class Dog extends Animal {
+    constructor() {
+        super("dog");
+    }
+
+    // Method to bark
+    bark(): void {
+        console.log("woof");
+    }
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
